88a78bfe87f6f167dff581cc946977015d200855
Daniel Lacasse <daniel@gradle.com>
2017-08-10 12:41:47 -0400

    Add sample demonstrating external dependencies

---
+++ cpp/external-dependency/src/main/cpp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "greeter.h"
+
+int main() {
+    std::cout << "The sum of 40 and 2 is " << sum(40, 2) << "!" << std::endl;
+    return 0;
+}
---
+++ swift/external-dependency/src/main/swift/main.swift
@@ -0,0 +1,4 @@
+import Greeter
+
+// Simple hello world app
+print("The sum of 40 and 2 is \(sum(a: 40, b: 2))!")
