614e5552d27442f4d9e97e0316afcc5232e61d69
Sterling Greene <sterling@gradle.com>
2017-10-10 10:37:59 -0400

    Fix swift simple-library sample

--- swift/simple-library/src/test/swift/sum_test.swift
+++ swift/simple-library/src/test/swift/sum_test.swift
@@ -1,21 +1,24 @@
 import XCTest
-import Greeter
+import Math
 
 class SumTest: XCTestCase {
     func testCanSumNumbersTo42() {
-        XCTAssertEqual(42, sum(a: 40, b: 2))
-        XCTAssertEqual(42, sum(a: 12, b: 30))
-        XCTAssertEqual(42, sum(a: 33, b: 9))
+        let main = Math()
+        XCTAssertEqual(42, main.sum(a: 40, b: 2))
+        XCTAssertEqual(42, main.sum(a: 12, b: 30))
+        XCTAssertEqual(42, main.sum(a: 33, b: 9))
     }
 
     func testIncorrectSums() {
-        XCTAssertNotEqual(42, sum(a: 11, b: 2))
-        XCTAssertNotEqual(42, sum(a: 1, b: 2))
+        let main = Math()
+        XCTAssertNotEqual(42, main.sum(a: 11, b: 2))
+        XCTAssertNotEqual(42, main.sum(a: 1, b: 2))
     }
 
     func testSumIsPerformant() {
+        let main = Math()
         self.measure {
-            sum(a: 40, b: 2)
+            main.sum(a: 40, b: 2)
         }
     }
 }
