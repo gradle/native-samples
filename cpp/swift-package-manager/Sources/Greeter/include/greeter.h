887baca6a96631d79ce047617195532a92106370
Daniel Lacasse <daniel@gradle.com>
2017-10-19 22:22:25 -0400

    Export more API for windows

--- cpp/swift-package-manager/Sources/Greeter/include/greeter.h
+++ cpp/swift-package-manager/Sources/Greeter/include/greeter.h
@@ -1,4 +1,16 @@
-class Greeter {
+#pragma once
+
+#ifdef _WIN32
+#  ifdef _DLL
+#    define GREETER_API __declspec(dllexport)
+#  else
+#    define GREETER_API __declspec(dllimport)
+#  endif
+#else
+#  define GREETER_API
+#endif
+
+class GREETER_API Greeter {
     public:
     void sayHello();
 };
--- cpp/transitive-dependencies/hello/src/main/public/hello.h
+++ cpp/transitive-dependencies/hello/src/main/public/hello.h
@@ -1,3 +1,13 @@
 #pragma once
 
-void sayHello();
+#ifdef _WIN32
+#  ifdef _DLL
+#    define HELLO_API __declspec(dllexport)
+#  else
+#    define HELLO_API __declspec(dllimport)
+#  endif
+#else
+#  define HELLO_API
+#endif
+
+HELLO_API void sayHello();
