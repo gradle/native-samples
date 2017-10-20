bad0cdcb9be40db378916dd592f5076cf5be1151
Daniel Lacasse <daniel@gradle.com>
2017-10-19 22:15:55 -0400

    Export API from simple library

--- cpp/simple-library/src/main/public/mathlib.h
+++ cpp/simple-library/src/main/public/mathlib.h
@@ -1,6 +1,16 @@
 #pragma once
 
-class Math {
+#ifdef _WIN32
+#  ifdef _DLL
+#    define MATH_API __declspec(dllexport)
+#  else
+#    define MATH_API __declspec(dllimport)
+#  endif
+#else
+#  define MATH_API
+#endif
+
+class MATH_API Math {
   public:
     int sum(int a, int b);
 };
