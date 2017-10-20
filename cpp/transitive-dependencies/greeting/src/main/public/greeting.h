fda55924dbbe169e21465a3a0f28b50b1707aeab
Daniel Lacasse <daniel@gradle.com>
2017-10-19 22:19:01 -0400

    Add export from greeting library

--- cpp/transitive-dependencies/greeting/src/main/public/greeting.h
+++ cpp/transitive-dependencies/greeting/src/main/public/greeting.h
@@ -2,4 +2,14 @@
 
 #include <string>
 
-std::string getHello();
+#ifdef _WIN32
+#  ifdef _DLL
+#    define GREETING_API __declspec(dllexport)
+#  else
+#    define GREETING_API __declspec(dllimport)
+#  endif
+#else
+#  define GREETING_API
+#endif
+
+GREETING_API std::string getHello();
