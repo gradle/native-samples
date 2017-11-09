ba17c579e80636c79705f3167c16d5779c79b83e
Daniel Lacasse <daniel@gradle.com>
2017-11-09 07:20:42 -0500

    Add XCTest Linux support for executables

--- swift/executable/src/test/swift/AppTests.swift
+++ swift/executable/src/test/swift/AppTests.swift
@@ -2,6 +2,10 @@ import XCTest
 @testable import App
 
 class AppTests: XCTestCase {
+    public static var allTests = [
+        ("testEqualsExpectedMessage", testEqualsExpectedMessage),
+    ]
+
     func testEqualsExpectedMessage() {
         XCTAssertEqual("Hello, World!", getMessage())
     }
--- swift/swift-package-manager/Tests/AppTests/AppTests.swift
+++ swift/swift-package-manager/Tests/AppTests/AppTests.swift
@@ -2,6 +2,10 @@ import XCTest
 @testable import App
 
 class AppTests: XCTestCase {
+    public static var allTests = [
+        ("testDummy", testDummy),
+    ]
+
     func testDummy() {
         XCTAssertTrue(true)
     }
