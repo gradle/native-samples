8e94bce96b845958a3d02fc972a16affce41d494
Daniel Lacasse <daniel@gradle.com>
2017-12-13 10:21:42 +1100

    Fix Swift application sample on CI
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- swift/application/src/main/swift/reverse.swift
+++ swift/application/src/main/swift/reverse.swift
@@ -1,6 +1,5 @@
 internal func reverse(source: String) -> String {
-    let characters = Array(source)
-    let reversedCharacters = characters.reversed()
+    let reversedCharacters = source.reversed()
     let reversedString = String(reversedCharacters)
 
     return reversedString
