02f365f741bd528f99f258e042f942f20d1d9b39
Daniel Lacasse <daniel@gradle.com>
2017-12-13 11:03:52 +1100

    Fix CI failure for C++ composite build sample
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- cpp/composite-build/src/main/cpp/main.cpp
+++ cpp/composite-build/src/main/cpp/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "mathlib.h"
-#include "hello.h"
+#include "string_utils.h"
 
 // Simple hello world app
 int main() {
-    sayHello();
+    std::string value = "Hello,    World!";
+    std::cout << "Convert '" << value << "' to use a single space: " << join(split(value)) << std::endl;
     Math math;
     std::cout << "The sum of 40 and 2 is " << math.sum(40, 2) << "!" << std::endl;
     return 0;
