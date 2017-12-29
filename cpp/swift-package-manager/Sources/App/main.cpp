1afe7ba1ef48fee549e603259e8e7ecb321e746c
Adam Murdoch <adam@gradle.com>
2017-12-29 14:48:08 +1100

    Reuse the template app for the C++ swift-package-manager sample.

--- cpp/swift-package-manager/Sources/App/main.cpp
+++ cpp/swift-package-manager/Sources/App/main.cpp
@@ -1,7 +1,12 @@
-#include "greeter.h"
+#include <iostream>
 
-int main(int argc, char** argv) {
-    Greeter greeter;
-    greeter.sayHello();
+#include "string_utils.h"
+#include "linked_list.h"
+
+// Simple hello world app
+int main() {
+    linked_list tokens;
+    tokens = split("Hello,      World!");
+    std::cout << join(tokens) << std::endl;
     return 0;
 }
--- cpp/swift-package-manager/Sources/Greeter/greeter.cpp
+++
@@ -1,7 +0,0 @@
-#define GREETER_MODULE_EXPORT
-#include "greeter.h"
-#include <iostream>
-
-void Greeter::sayHello() {
-    std::cout << "Hello world" << std::endl;
-}
