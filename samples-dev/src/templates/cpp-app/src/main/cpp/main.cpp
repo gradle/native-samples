94e2ffb65831f07cd6c8382ef21920da8529bbc3
Adam Murdoch <adam@gradle.com>
2017-12-29 12:54:13 +1100

    Updated the template C++ app

--- samples-dev/src/templates/cpp-app/src/main/cpp/main.cpp
+++ samples-dev/src/templates/cpp-app/src/main/cpp/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 
 #include "string_utils.h"
-
-// 'linked_list.h' is exposed through transitivity.
 #include "linked_list.h"
-
-// `node.h` is an implementation detail, aka private, headers of the linked_list project.
-// It cannot be included here.
-// #include "node.h"
 
 // Simple hello world app
 int main() {
