ced34f75163fe0827cda5ed0350c43a647653ae8
Adam Murdoch <adam@gradle.com>
2018-01-16 18:42:54 +1000

    Use different module names for the macOS and Linux specific Swift libraries.

--- samples-dev/src/templates/swift-app-uses-logger/src/main/swift/main.swift
+++ samples-dev/src/templates/swift-app-uses-logger/src/main/swift/main.swift
@@ -2,7 +2,11 @@
  * A simple hello world application. Uses a library to tokenize and join a string and prints the result.
  */
 import Utilities
-import Logger
+#if os(Linux)
+    import LinuxLogger
+#else
+    import MacOsLogger
+#endif
 
 class App {
     class func normalize(_ s: String) -> String {
--- swift/operating-system-specific-dependencies/app/src/main/swift/main.swift
+++ swift/operating-system-specific-dependencies/app/src/main/swift/main.swift
@@ -1,7 +1,11 @@
 /*
  * A simple hello world application. Uses a library to tokenize and join a string and prints the result.
  */
-import Logger
+#if os(Linux)
+    import LinuxLogger
+#else
+    import MacOsLogger
+#endif
 
 class App {
     class func normalize(_ s: String) -> String {
