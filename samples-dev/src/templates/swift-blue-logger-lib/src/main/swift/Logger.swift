22cf61926712d8388b6d2bd407522dea79a0e46a
Adam Murdoch <adam@gradle.com>
2018-01-16 18:58:44 +1000

    Changed Swift logger libraries to not colorize the output when not connected to a terminal.

--- samples-dev/src/templates/swift-blue-logger-lib/src/main/swift/Logger.swift
+++ samples-dev/src/templates/swift-blue-logger-lib/src/main/swift/Logger.swift
@@ -1,5 +1,15 @@
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
+
 public class Logger {
     public class func info(_ s: String) {
-        print("\u{001B}[0;34m" + s + "\u{001B}[0m")
+        if (isatty(1) != 0) {
+            print("\u{001B}[0;34m" + s + "\u{001B}[0m")
+        } else {
+            print(s)
+        }
     }
 }
