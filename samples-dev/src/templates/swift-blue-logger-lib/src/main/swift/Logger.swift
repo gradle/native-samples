c08e29f6198aa900fd38418520972838239e2899
Adam Murdoch <adam@gradle.com>
2018-01-16 18:43:46 +1000

    Changed the Swift logging libraries to reset the console text attributes after loging the message.

--- samples-dev/src/templates/swift-blue-logger-lib/src/main/swift/Logger.swift
+++ samples-dev/src/templates/swift-blue-logger-lib/src/main/swift/Logger.swift
@@ -1,5 +1,5 @@
 public class Logger {
     public class func info(_ s: String) {
-        print("\u{001B}[0;34m" + s)
+        print("\u{001B}[0;34m" + s + "\u{001B}[0m")
     }
 }
--- swift/operating-system-specific-dependencies/green-logger/src/main/swift/Logger.swift
+++ swift/operating-system-specific-dependencies/green-logger/src/main/swift/Logger.swift
@@ -1,5 +1,5 @@
 public class Logger {
     public class func info(_ s: String) {
-        print("\u{001B}[0;32m" + s)
+        print("\u{001B}[0;32m" + s + "\u{001B}[0m")
     }
 }
