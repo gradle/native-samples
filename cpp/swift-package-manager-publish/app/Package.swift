6e67eecb4b2728e8db7697999573be5bfe0fb6a1
Adam Murdoch <adam@gradle.com>
2018-01-30 10:21:39 +1100

    Updated samples to a new nightly.

--- cpp/swift-package-manager-publish/app/Package.swift
+++ cpp/swift-package-manager-publish/app/Package.swift
@@ -5,13 +5,15 @@ import PackageDescription
 let package = Package(
     name: "App",
     dependencies: [
-        .package(url: "../utilities-library", from: "1.0.0")
+        .package(url: "../utilities-library", from: "1.0.0"),
+        .package(url: "../list-library", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "utilities")
+                .product(name: "utilities"),
+                .product(name: "list")
             ]
         ),
     ]
