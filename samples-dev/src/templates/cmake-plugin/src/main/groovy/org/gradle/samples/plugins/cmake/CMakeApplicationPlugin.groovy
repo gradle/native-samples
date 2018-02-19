5fe7cc4fef5bc581e7468b118e8ebf116ab4161e
Sterling Greene <sterling@gradle.com>
2018-02-18 21:02:02 -0800

    Move CMake plugins into a cmake package

--- samples-dev/src/templates/cmake-plugin/src/main/groovy/org/gradle/samples/plugins/cmake/CMakeApplicationPlugin.groovy
+++ samples-dev/src/templates/cmake-plugin/src/main/groovy/org/gradle/samples/plugins/cmake/CMakeApplicationPlugin.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.samples
+package org.gradle.samples.plugins.cmake
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
--- samples-dev/src/templates/cmake-plugin/src/main/groovy/org/gradle/samples/plugins/cmake/CMakeExtension.groovy
+++ samples-dev/src/templates/cmake-plugin/src/main/groovy/org/gradle/samples/plugins/cmake/CMakeExtension.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.samples
+package org.gradle.samples.plugins.cmake
 
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.ProjectLayout
