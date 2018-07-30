0d7cee810fc8a815542f76783fc57ed8462e35ce
Adam Murdoch <adam@gradle.com>
2018-07-30 12:25:43 +1000

    Convert some samples dev build scripts to Kotlin DSL. None of the samples have changed.

---
+++ samples-dev/plugins/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java-library")
+    id("groovy")
+    id("java-gradle-plugin")
+    id("org.jetbrains.kotlin.jvm").version("1.2.51")
+}
+
+group = "org.gradle.samples.plugins"
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(localGroovy())
+    implementation("org.eclipse.jgit:org.eclipse.jgit:4.9.1.201712030800-r")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+gradlePlugin {
+    plugins {
+        create("wrapper") {
+            id = "org.gradle.samples.wrapper"
+            implementationClass = "org.gradle.samples.plugins.wrapper.WrapperPlugin"
+        }
+    }
+}
