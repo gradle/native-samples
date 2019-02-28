950c53016153deac73dc1fecb21946accdafbc6a
Eric Wendelin <eric@gradle.com>
2019-02-28 16:56:49 -0700

    Bootstrap use of common Gradle docs infrastructure
    This change introduces Asciidoctor, Asciidoctor extensions, and
    Exemplar to generate a docs/ site with a mini-guide for each
    sample.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,47 @@
+import org.asciidoctor.gradle.AsciidoctorTask
+
 // This root project is simply a container of sample builds
 plugins {
+    `java-library`
+    `kotlin-dsl`
+    id("org.asciidoctor.convert") version "1.5.3"
     id("org.gradle.samples.wrapper")
+}
+
+repositories {
+    jcenter()
+    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+}
+
+dependencies {
+    testImplementation("org.gradle:sample-check:0.7.0")
+    testImplementation(gradleTestKit())
+    asciidoctor("org.gradle:docs-asciidoctor-extensions:0.6.0")
+}
+
+tasks {
+    getByName<AsciidoctorTask>("asciidoctor") {
+        inputs.dir("cpp/application")
+        sourceDir = file("cpp/application")
+        outputDir = file("docs")
+        backends("html5")
+
+        attributes(
+                mapOf("source-highlighter" to "prettify",
+                        "imagesdir" to "images",
+                        "stylesheet" to null,
+                        "linkcss" to true,
+                        "docinfodir" to ".",
+                        "docinfo1" to "",
+                        "nofooter" to true,
+                        "icons" to "font",
+                        "sectanchors" to true,
+                        "sectlinks" to true,
+                        "linkattrs" to true,
+                        "encoding" to "utf-8",
+                        "idprefix" to "",
+                        "toc" to "auto",
+                        "toclevels" to 1)
+        )
+    }
 }
